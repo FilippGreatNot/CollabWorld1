e0f9b06c0b9a3db050502d45792bbbfba659aa5c
Manenderrr <Manendeer@gmail.com>
2024-12-26 18:12:39 +0500

    I actually pushed nothing important but whatever

--- common/defines/cw_defines.lua
+++ common/defines/cw_defines.lua
@@ -1,2 +1,6 @@
 NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 99999.0
--- ^ This stops the AI form making wargoals on people they dont like and messing up the game ^
+--[[
+So basically CPU starts randomly justifying on countries it dislikes when reaching 100 threat. This makes it never do that no matter what
+I cannot explain that any further since I don't actually undestrand how does this mechanic work at all. HoI4 is kinda weird like this
+- Manender
+--]]
